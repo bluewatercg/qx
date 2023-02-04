fcca23fea2d45cf61227cbb1b81ee699fe09f147
bluewatercg <bluewater_cg@126.com>
2023-02-04 15:02:03 +0800

    Update getCookie.js

--- getCookie.js
+++ getCookie.js
@@ -1,1 +1,18 @@
-if($request.url.indexOf("zhiyoo" && "qiandao") != -1){getCookie("智友社区", "zyC")}function getCookie(name, key){  var currentC = $request.headers.Cookie;  var existC = key => $prefs.valueForKey(key);  var create = (currentC, key) => $prefs.setValueForKey(currentC, key);  if(currentC && existC(key)){    currentC != existC(key) ?    (create(currentC, key),    $notify(name, "更新Cookie成功", "")) :    console.log(name + "目前的cookie一致")    }else{        currentC != undefined ?         (create(currentC, key),        $notify(name, "获取cookie成功", "")) :         $notify(name, "获取Cookie失败", "")    }}$done({})
+if ($request.url.indexOf("zhiyoo" && "qiandao") != -1) {
+  getCookie("智友社区", "zyC");
+}
+function getCookie(name, key) {
+  var currentC = $request.headers.Cookie;
+  var existC = (key) => $prefs.valueForKey(key);
+  var create = (currentC, key) => $prefs.setValueForKey(currentC, key);
+  if (currentC && existC(key)) {
+    currentC != existC(key)
+      ? (create(currentC, key), $notify(name, "更新Cookie成功", ""))
+      : console.log(name + "目前的cookie一致");
+  } else {
+    currentC != undefined
+      ? (create(currentC, key), $notify(name, "获取cookie成功", ""))
+      : $notify(name, "获取Cookie失败", "");
+  }
+}
+$done({});
